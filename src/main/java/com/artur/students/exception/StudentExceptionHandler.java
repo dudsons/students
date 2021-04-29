4ced03cdde35c7549e534817f14e483f5bbd8bed
rydzewskima <postgres>
2021-04-29 14:36:26 +0200

    Handled exception when wrong value in http request;

--- src/main/java/com/artur/students/exception/StudentExceptionHandler.java
+++ src/main/java/com/artur/students/exception/StudentExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.artur.students.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,11 +18,17 @@ public class StudentExceptionHandler {
             httpStatus = HttpStatus.NOT_FOUND;
         } else if (StudentError.EMAIL_IS_EXIST.equals(studentException.getStudentError())) {
             httpStatus = HttpStatus.CONFLICT;
-        } else if (StudentError.STUDENT_NOT_ACTIVE.equals(studentException.getStudentError())){
+        } else if (StudentError.STUDENT_NOT_ACTIVE.equals(studentException.getStudentError())) {
             httpStatus = HttpStatus.BAD_REQUEST;
         }
 
         return new ResponseEntity<>(new ErrorInfo(studentException.getStudentError().getMessage()), httpStatus);
+    }
+
+    @ExceptionHandler
+    ResponseEntity<ErrorInfo> wrongValueException(HttpMessageNotReadableException httpMessageNotReadableException) {
+        InvalidFormatException invalidFormatException = (InvalidFormatException) httpMessageNotReadableException.getCause();
+        return new ResponseEntity<>(new ErrorInfo("Wrong value " + "\"" + invalidFormatException.getValue() + "\"" + " in HTTP request"), HttpStatus.BAD_REQUEST);
     }
 }
 
